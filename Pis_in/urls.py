2b0bf9e832e33bc5c7df3f44cd79f26864cbc618
Medina23334444 <luis253363@gmail.com>
2024-07-17 14:21:22 -0500

    Corregir Modelo Iniciar Sesion, Agregar Sugerencias

--- Pis_in/urls.py
+++ Pis_in/urls.py
@@ -5,7 +5,7 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('manage-user/', views.manage_user, name='manage_user'),
     path('admiManage/', views.admiManage, name='admiManage'),
     path('iniciarSesion/', views.iniciarSesion, name='iniciarSesion'),
@@ -15,6 +15,7 @@ urlpatterns = [
     path('homeAdministrador/', views.homeAdministrador, name='homeAdministrador'),
     path('perfilAdministrador/', views.perfilAdministrador, name='perfilAdministrador'),
     path('homePersonal/', views.homePersonal, name='homePersonal'),
-    path('ordenarUsuarios/', views.ordenarUsuarios)
+    path('ordenarUsuarios/', views.ordenarUsuarios),
+    path('cerrar_sesion', views.cerrarSesion, name='cerrar_sesion'),
 ]
 

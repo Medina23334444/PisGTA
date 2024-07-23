3daa03ce3e455adcf9d354e4cbe1f3ae491a92ee
alexlude2004 <alex.gradyn@gmail.com>
2024-07-23 11:51:09 -0500

    ExportarPDF - new

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-    from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError, transaction
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout

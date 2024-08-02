091e653e3a9f582f673d29c8c9721cbad6c5a717
alexlude2004 <alex.gradyn@gmail.com>
2024-08-01 19:26:31 -0500

    correcciones en el PDF

--- static/scriptExportarPDF.js
+++ static/scriptExportarPDF.js
@@ -1,21 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const showContainerBtn = document.getElementById("showContainerBtn");
+    const closeContainerBtn = document.getElementById("cancelBtn");
+    const exportContainer = document.getElementById("exportContainer");
+    const fileName = document.getElementById("fileName");
+
+    showContainerBtn.addEventListener("click", function () {
+        exportContainer.style.display = "block";
+        fileName.value = '';
+        fileName.placeholder = "Ingrese el nombre del archivo";
+    });
+
+    closeContainerBtn.addEventListener("click", function () {
+        exportContainer.style.display = "none";
+        fileName.value = '';
+        fileName.placeholder = "Ingrese el nombre del archivo";
+    });
+});
+
+document.getElementById("predictionButton").addEventListener("click", function() {
+    var select = document.getElementById("yearSelect");
+    var texto = document.getElementById("textoPrediccion");
+    var anio = select.options[select.selectedIndex].text;
+    texto.innerHTML = "Predicción Gráfica para el año " + anio;
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     const fileNameInput = document.getElementById('fileName');
     const exportBtn = document.getElementById('exportBtn');
-    const exportContainer = document.getElementById('exportContainer');
 
     const initChart = async () => {
         const myChart = echarts.init(document.getElementById('chart'));
-    
+
         const option = await getObtionChart();
-    
-        option.toolbox = {
-            feature: {
-                saveAsImage: {
-                    show: false 
-                }
-            }
+
+        option.title = {
+            show: false
         };
-    
+
+        option.toolbox = {
+            show: false
+        };
+
         myChart.setOption(option);
         myChart.resize();
     };
@@ -28,19 +53,24 @@ document.addEventListener('DOMContentLoaded', function () {
         const img = document.createElement('img');
         img.src = chartImage;
         img.style.width = '800px';
-        img.style.marginTop = '35px';
+        img.style.marginTop = '-25px';
         img.style.marginBottom = '25px';
-        img.style.marginLeft = '25px';
-        img.style.height = '400px' ;
+        img.style.height = '400px';
 
         const chartImageContainer = document.getElementById('chartImageContainer');
-        chartImageContainer.innerHTML = ''; 
+        chartImageContainer.innerHTML = '';
         chartImageContainer.appendChild(img);
     };
 
     $('#exportContainer').on('show.bs.modal', async function () {
         await captureChart();
     });
+
+    const dias_Semana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const fecha = new Date();
+    const dia = dias_Semana[fecha.getDay()];
+    document.getElementById("fecha_actual").innerHTML = `${dia}, ${fecha.toLocaleDateString()}`;
+
 
     exportBtn.addEventListener('click', function () {
         const fileName = fileNameInput.value.trim();
@@ -76,6 +106,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         };
 
+        // Generar el PDF
         html2pdf().set(opt).from(content).save().then(function () {
             console.log('PDF generado y guardado');
             $('#exportContainer').modal('hide');
@@ -87,3 +118,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('load', initChart);
 });
+
